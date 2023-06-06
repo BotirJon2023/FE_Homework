73557cfd5a289588408b84dfc81014d764b3231b
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-06-06 04:08:49 +0200

    Add files via upload
    HomeWork with JavaScript 05.06.2023

---
+++ ДЗ_6_05.06.23/JS/script.js
@@ -0,0 +1,47 @@
+
+// let number_1 = +prompt("Введите число")
+// let sum = number_1 * 0.10
+// console.log(`${sum} - это 10% от введенного Вами числа`)
+
+
+// let number_1 = +prompt("Введите первое число")
+// let number_2 = +prompt("Введите второе число")
+// if (number_1 < number_2) {
+//    console.log(number_1)
+// }
+// else {
+//    console.log(number_2)
+// }
+
+
+
+//let number_1 = +prompt("Введите число")
+//if (number_1 < 100) {
+//    console.log(`Число ${number_1} меньше 100`)
+//}
+//else if (number_1 > 100) {
+//    console.log(`Число ${number_1} больше 100`)
+//}
+//else if (number_1 == 100) {
+//    console.log(`Число ${number_1} равно 100`)
+//}
+
+
+
+const username = prompt("Введите ваше имя")
+const userage = +prompt("Введите год рождения") 
+
+if (2023 - userage >= 18) {
+    console.log(`Hello, ${username}`)
+}
+
+    else if (2023 - userage < 18) {
+        console.log(`Hi, ${username}`)
+    }
+
+
+
+
+// console.log("Привет, " + username + ". Вам " + userage + " лет")
+// console.log(`Привет, ${username}. Вам ${userage} лет`)
+
