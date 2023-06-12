b853822ef71b3ecdcc793a1ca84f967bc7c84e02
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-06-12 13:20:04 +0200

    Add files via upload
    FE_HomeWork_07.06.23

---
+++ ДЗ_7_07.06.23/JS/script.js
@@ -0,0 +1,55 @@
+// ДЗ 1.1.
+// let num; 
+//     for (num = 0; num <= 10; num++) {
+//         if (num % 2 ==0)
+//         console.log(num)
+//     }
+
+// ДЗ 1.2.
+
+// for (i = 20; i <= 55; i++) {
+//     if (i % 10 == 0 || i % 10 >= 7 ) {
+//       continue;
+//     }
+//     console.log(i);
+//   }
+
+// ДЗ 1.3.
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6];
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i])
+// }
+
+
+//  ДЗ 1.4.
+
+// const numbers_squared = []
+// for (let i = 0; i < numbers.length; i++) {
+//     numbers_squared.push(numbers[i]**2)
+// }
+// console.log(numbers_squared)
+
+
+// ДЗ 1.5
+
+// const last_elem = numbers_squared [numbers_squared.length-1]         //Curly brackets,{}
+
+// console.log(last_elem)
+
+
+// ДЗ 1.6
+
+// const user = {
+//     first_name:'Ivan',
+//     last_name: 'Ivanov',
+//     age:20,
+//     salary:500
+// }
+// console.log(users name is: ${user.first_name} ${user.last_name} He is ${user.age} years old)
+
+  
+
+
+
+  
