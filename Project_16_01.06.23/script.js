874e0a7a3328eb1e9d31da0519f3720b3359a58f
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-07-10 12:19:27 +0200

    Add files via upload
    Learning FrontEnd: HTML, JS, CSS

---
+++ Project_16_01.06.23/script.js
@@ -0,0 +1,26 @@
+alert ("Hello!")
+
+console.log("hello")
+
+prompt("Как тебя зовут?")
+
+const name = prompt("Как тебя зовут?")
+
+console.log(name)
+
+const userAge = 40
+const userLogin = "Starta institute"
+
+console.log(userAge)
+console.log("userAge")
+
+
+const productName = "Bred"
+const userName = "Michail"
+const productPrice = "100"
+
+console.log(productName)
+console.log(userName)
+console.log(productPrice)
+
+/* console.log(productName, userName, productPrice) */
