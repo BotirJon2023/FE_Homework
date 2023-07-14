884621e7d3464f026ae65ce8ddba675c2bd460b6
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-07-14 08:52:27 +0200

    Add files via upload
    Learning FrontEnd: HTML, JS and CSS

---
+++ Project_17_02.06.23/js/scrpict.js
@@ -0,0 +1,53 @@
+// let number = 10;
+// let result = number **4;
+
+// console.log(result)
+// alert(result)
+
+// let radius = +prompt('Введите радиус')
+
+// console.log(radius)
+
+// let result = Math.PI * radius ** 2
+
+// console.log(result)
+
+// let temp_c = +prompt();
+// let result = temp_c * 1,8 + 32;
+
+// console.log(result);
+
+// let time = +prompt();
+
+// let result = time / 24
+
+// console.log(result)
+
+// alert(888 % 24)
+
+
+// let order_cost = 1000;
+// let discount = .15;
+// alert(order_cost * (1 - discount))
+
+// let product_price = 100;
+
+// console. log(product_price * .8);
+// console. log(product_price * 1.105);
+
+// let number1 = +prompt('Введите первое число')
+// let number2 = +prompt('Введите второе число')
+// let number3 = +prompt('Введите третье число')
+
+// let result = (number1 + number2 + number3) / 3
+
+// console.log(result)
+
+// let number = +prompt('Введите любое число')
+// let result = number - (1 * .01)
+// console.log(result)
+
+
+console. log(proguct_price )
+let result = number - (1 * .01)
+console.log(result)
