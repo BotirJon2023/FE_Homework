8c362e47b0a0cb50872023ff01a0098358f5627c
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-08-18 06:34:01 +0200

    Add files via upload
    Learning FrontEnd: HTML, CSS, JS

---
+++ Project_23_10.06.23/JS/script.js
@@ -0,0 +1,12 @@
+// HomeWork
+
+// let num;
+// for (num = 0; num <= 10; num++) {
+//     if (num % 2 ==0)
+  
+//   console.log(num);
+// }
+
+for (let i = 20; i <= 55; i++) {
+  console.log(i);
+}
